7417c119c6cfe695139c24583e6ae57a9268a639
Hemanth Savarala <hemanths@live.com>
2024-07-15 01:28:48 +0530

    Fix toggle

--- lib/features/settings/presentation/widgets/true_black_widget_theme.dart
+++ lib/features/settings/presentation/widgets/true_black_widget_theme.dart
@@ -1,37 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
 
 import 'package:paisa/config/routes.dart';
 
 import 'package:paisa/core/common.dart';
 import 'package:paisa/core/widgets/paisa_widget.dart';
 
-class JustBlackWidget extends StatefulWidget {
+class JustBlackWidget extends StatelessWidget {
   const JustBlackWidget({super.key});
 
   @override
-  State<JustBlackWidget> createState() => _JustBlackWidgetState();
-}
-
-class _JustBlackWidgetState extends State<JustBlackWidget> {
-  @override
   Widget build(BuildContext context) {
-    final index = settings.get(themeModeKey, defaultValue: 0);
-    late ThemeMode themeMode = ThemeMode.values[index];
-    if (themeMode == ThemeMode.light || themeMode == ThemeMode.system) {
-      return const SizedBox.shrink();
-    } else {
-      return Column(
-        children: [
-          SwitchListTile(
-            activeColor: context.secondary,
-            secondary: const SizedBox.shrink(),
-            title: Text(context.loc.justBlackText),
-            onChanged: (bool value) => settings.put(blackThemeKey, value),
-            value: settings.get(blackThemeKey, defaultValue: false),
-          ),
-          const PaisaDivider()
-        ],
-      );
-    }
+    return ValueListenableBuilder<Box<dynamic>>(
+      valueListenable: settings.listenable(keys: [themeModeKey, blackThemeKey]),
+      builder: (context, value, child) {
+        final themeIndex = value.get(themeModeKey, defaultValue: 0);
+        final blackTheme = value.get(blackThemeKey, defaultValue: false);
+        late ThemeMode themeMode = ThemeMode.values[themeIndex];
+        if (themeMode == ThemeMode.light || themeMode == ThemeMode.system) {
+          return const SizedBox.shrink();
+        } else {
+          return Column(
+            children: [
+              SwitchListTile(
+                activeColor: context.secondary,
+                secondary: const SizedBox.shrink(),
+                title: Text(context.loc.justBlackText),
+                onChanged: (bool value) => settings.put(blackThemeKey, value),
+                value: blackTheme,
+              ),
+              const PaisaDivider()
+            ],
+          );
+        }
+      },
+    );
   }
 }
